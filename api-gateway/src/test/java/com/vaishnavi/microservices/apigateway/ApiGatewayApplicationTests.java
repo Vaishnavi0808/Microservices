d70950a39d8df10d6529d8fe7136b9f063856ccd
Vaishnavi Sarode <sarodevaishnavi3@gmail.com>
2023-05-31 01:49:22 +0530

    Add files via upload

---
+++ api-gateway/src/test/java/com/vaishnavi/microservices/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vaishnavi.microservices.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
